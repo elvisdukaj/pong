a385fe7696cf0c75284b31bb8c1edaca5a506981
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-03-21 08:56:42 +0100

    vis:utility: fix wrong random_direction

--- vis/lib/utility.cpp
+++ vis/lib/utility.cpp
@@ -22,9 +22,9 @@ inline vec2 get_random_direction(vec2 vec, float min_angle, float max_angle) {
 	float a = get_random(min_angle, max_angle);
 
 	mat4 rot = gtc::identity<mat4>();
-	rot = gtc::rotate(rot, a, vec3{vec.x, vec.y, 0.0f});
+	rot = gtc::rotate(rot, a, vec3{vec.x, vec.y, 1.0f});
 	auto rotated = vec4{vec.x, vec.y, 0.0f, 1.0f} * rot;
 
-	return {rotated.x, rotated.y};
+	return normalize(vec2{rotated.x, rotated.y});
 }
 } // namespace vis
