c195b76f7d3d4f92d3ed3da608ffbf67be3f0aa4
Elvis Dukaj <elvis.dukaj@dynatrace.com>
2025-04-17 14:10:00 +0200

    vis:window: splitting between interface and implementation

--- vis/lib/window/window.cpp
+++ vis/lib/window/window.cpp
@@ -1,7 +1,29 @@
 module;
 
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_vulkan.h>
+/* Avoid including vulkan.h, don't define VkInstance if it's already included */
+#ifdef VULKAN_H_
+#define NO_SDL_VULKAN_TYPEDEFS
+#endif
+#ifndef NO_SDL_VULKAN_TYPEDEFS
+#define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
+
+#if defined(__LP64__) || defined(_WIN64) || defined(__x86_64__) || defined(_M_X64) || defined(__ia64) ||               \
+		defined(_M_IA64) || defined(__aarch64__) || defined(__powerpc64__)
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object##_T* object;
+#else
+#define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef uint64_t object;
+#endif
+
+VK_DEFINE_HANDLE(VkInstance)
+VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkSurfaceKHR)
+struct VkAllocationCallbacks;
+#endif
+#undef VK_DEFINE_HANDLE
+#undef VK_DEFINE_NON_DISPATCHABLE_HANDLE
+
+extern "C" {
+struct SDL_Window;
+}
 
 export module vis.window;
 
@@ -9,106 +31,59 @@ import std;
 
 export namespace vis {
 
-enum class WindowsFlags : uint64_t {
-	fullscreen = SDL_WINDOW_FULLSCREEN,
-	opengl = SDL_WINDOW_OPENGL,
-	occluded = SDL_WINDOW_OCCLUDED,
-	hidden = SDL_WINDOW_HIDDEN,
-	borderless = SDL_WINDOW_BORDERLESS,
-	resizable = SDL_WINDOW_RESIZABLE,
-	minimized = SDL_WINDOW_MINIMIZED,
-	maximized = SDL_WINDOW_MAXIMIZED,
-	grabbed = SDL_WINDOW_MOUSE_GRABBED,
-	input_focus = SDL_WINDOW_INPUT_FOCUS,
-	mouse_focus = SDL_WINDOW_MOUSE_FOCUS,
-	external = SDL_WINDOW_EXTERNAL,
-	high_pixel_density = SDL_WINDOW_HIGH_PIXEL_DENSITY,
-	modal = SDL_WINDOW_MODAL,
-	mouse_capture = SDL_WINDOW_MOUSE_CAPTURE,
-	mouse_relative_mode = SDL_WINDOW_MOUSE_RELATIVE_MODE,
-	always_on_top = SDL_WINDOW_ALWAYS_ON_TOP,
-	utility = SDL_WINDOW_UTILITY,
-	tooltip = SDL_WINDOW_TOOLTIP,
-	popup_menu = SDL_WINDOW_POPUP_MENU,
-	keyboard_grabbed = SDL_WINDOW_KEYBOARD_GRABBED,
-	vulkan = SDL_WINDOW_VULKAN,
-	metal = SDL_WINDOW_METAL,
-	transparent = SDL_WINDOW_TRANSPARENT,
-	not_focusable = SDL_WINDOW_NOT_FOCUSABLE,
-	window_pos_undefined = SDL_WINDOWPOS_UNDEFINED,
-	window_pos_centered = SDL_WINDOWPOS_CENTERED,
+enum class WindowsFlags : std::uint64_t {
+	fullscreen = 0x1,
+	opengl = 0x02,
+	occluded = 0x04,
+	hidden = 0x08,
+	borderless = 0x10,
+	resizable = 0x20,
+	minimized = 0x40,
+	maximized = 0x80,
+	grabbed = 0x100,
+	input_focus = 0x200,
+	mouse_focus = 0x400,
+	external = 0x800,
+	high_pixel_density = 0x1000,
+	modal = 0x2000,
+	mouse_capture = 0x4000,
+	mouse_relative_mode = 0x8000,
+	always_on_top = 0x10000,
+	utility = 0x20000,
+	tooltip = 0x40000,
+	popup_menu = 0x80000,
+	keyboard_grabbed = 0x100000,
+	vulkan = 0x0000000010000000,
+	metal = 0x400000,
+	transparent = 0x0000000040000000,
+	not_focusable = 0x0000000080000000,
 };
-
-constexpr WindowsFlags operator|(WindowsFlags lhs, WindowsFlags rhs) {
-	using underlying = std::underlying_type<WindowsFlags>::type;
-	return static_cast<WindowsFlags>(static_cast<underlying>(lhs) | static_cast<underlying>(rhs));
-}
 
 class Window {
 public:
-	static std::expected<Window, std::string> create(std::string_view title, int width, int height, WindowsFlags flags) {
-		using underlying = std::underlying_type<WindowsFlags>::type;
-		auto window = SDL_CreateWindow(title.data(), width, height, static_cast<underlying>(flags));
-		if (not window)
-			return std::unexpected(std::format("Unable to create window: {}", SDL_GetError()));
+	static std::expected<Window, std::string> create(std::string_view title, int width, int height, WindowsFlags flags);
 
-		return Window{window};
-	}
-
-	~Window() {
-		if (window) {
-			SDL_DestroyWindow(window);
-			window = nullptr;
-		}
-	}
-
-	friend void swap(Window& lhs, Window& rhs) {
-		std::swap(lhs.window, rhs.window);
-	}
+	~Window();
 
 	Window(Window&) = delete;
 	Window& operator=(Window&) = delete;
 
-	Window(Window&& rhs) noexcept : window{nullptr} {
-		swap(*this, rhs);
-	}
+	Window(Window&&);
+	Window& operator=(Window&& rhs);
 
-	Window& operator=(Window&& rhs) noexcept {
-		swap(*this, rhs);
-		return *this;
-	};
+	VkSurfaceKHR create_renderer_surface(VkInstance instance, const VkAllocationCallbacks* allocator) const;
+	std::vector<const char*> get_required_renderer_extension();
 
-	explicit operator SDL_Window*() const {
+	explicit operator SDL_Window*() const noexcept {
 		return window;
 	}
 
-	VkSurfaceKHR create_renderer_surface(VkInstance instance, const VkAllocationCallbacks* allocator) const {
-		VkSurfaceKHR surface;
-		if (not SDL_Vulkan_CreateSurface(window, instance, allocator, &surface)) {
-			throw std::runtime_error("Unable to create the Vulkan Surface");
-		}
-
-		return surface;
-	}
-
-	std::vector<const char*> get_required_renderer_extension() {
-		Uint32 count_instance_extensions;
-		const char* const* instance_extensions = SDL_Vulkan_GetInstanceExtensions(&count_instance_extensions);
-
-		std::vector<const char*> required_windows_extensions;
-		for (auto i = 0u; i != count_instance_extensions; ++i)
-			required_windows_extensions.emplace_back(instance_extensions[i]);
-
-		return required_windows_extensions;
-	}
+private:
+	friend void swap(Window& lhs, Window& rhs);
+	explicit Window(SDL_Window* window);
 
 private:
-	explicit Window(SDL_Window* window) : window(window) {
-		SDL_ShowWindow(window);
-	}
-
-private:
-	SDL_Window* window;
+	SDL_Window* window = nullptr;
 };
 
 } // namespace vis
@@ -117,7 +92,13 @@ export namespace vis::win {
 
 enum class Pressed : bool { no = false, yes = true };
 enum class Repeated : bool { no = false, yes = true };
-enum class VirtualKey { up = SDLK_UP, down = SDLK_DOWN, left = SDLK_LEFT, right = SDLK_RIGHT, escape = SDLK_ESCAPE };
+enum class VirtualKey : std::uint64_t {
+	up = 0x40000052u,
+	down = 0x40000051u,
+	left = 0x40000050u,
+	right = 0x4000004fu,
+	escape = 0x0000001bu,
+};
 enum class KeyMode : std::uint32_t {};
 
 struct KeyboardKeyDownEvent {
@@ -148,5 +129,3 @@ struct WindowsResized {
 using Event = std::variant<KeyboardKeyDownEvent, KeyboardKeyUpEvent, QuitEvent, WindowsResized, NullEvent>;
 
 } // namespace vis::win
-
-using vis::operator|;
---
+++ vis/lib/window/window_impl.cpp
@@ -0,0 +1,63 @@
+module;
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+
+module vis.window;
+
+import std;
+
+namespace vis {
+
+std::expected<Window, std::string> Window::create(std::string_view title, int width, int height, WindowsFlags flags) {
+	using underlying = std::underlying_type<WindowsFlags>::type;
+	auto window = SDL_CreateWindow(title.data(), width, height, static_cast<underlying>(flags));
+	if (not window)
+		return std::unexpected(std::format("Unable to create window: {}", SDL_GetError()));
+
+	return Window{window};
+}
+
+Window::Window(SDL_Window* window) : window{window} {}
+
+void swap(Window& lhs, Window& rhs) {
+	::std::swap(lhs.window, rhs.window);
+}
+
+Window::Window(Window&& other) : window{nullptr} {
+	swap(*this, other);
+}
+
+Window& Window::operator=(Window&& other) {
+	swap(*this, other);
+	return *this;
+}
+
+Window::~Window() {
+	if (window) {
+		SDL_DestroyWindow(window);
+		window = nullptr;
+	}
+}
+
+VkSurfaceKHR Window::create_renderer_surface(VkInstance instance, const VkAllocationCallbacks* allocator) const {
+	VkSurfaceKHR surface;
+	if (not SDL_Vulkan_CreateSurface(window, instance, allocator, &surface)) {
+		throw std::runtime_error("Unable to create the Vulkan Surface");
+	}
+
+	return surface;
+}
+
+std::vector<const char*> Window::get_required_renderer_extension() {
+	Uint32 count_instance_extensions;
+	const char* const* instance_extensions = SDL_Vulkan_GetInstanceExtensions(&count_instance_extensions);
+
+	std::vector<const char*> required_windows_extensions;
+	for (auto i = 0u; i != count_instance_extensions; ++i)
+		required_windows_extensions.emplace_back(instance_extensions[i]);
+
+	return required_windows_extensions;
+}
+
+} // namespace vis
